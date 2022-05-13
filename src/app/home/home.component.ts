ad404d7967763c46a43815da892c020fffb627d6
JunHwan Kim <nefertiri972@gmail.com>
2022-05-13 16:15:15 +0900

    Add Observable operators
    - Observable 에서 방출된 데이터를 가공할 필요가 있을 경우 Operator를 사용하여 가공할 수 있음

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Observable, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -19,12 +20,12 @@ export class HomeComponent implements OnInit, OnDestroy {
     const customIntervalObservable = new Observable(observer => {
       let count = 0;
       let intervalId = setInterval(() => {
-        console.log('Interval: ' + count);
+        //console.log('Interval: ' + count);
+        observer.next(count);
         if (count == 5) {
           observer.complete();
           //clearInterval(intervalId);
         }
-        observer.next(count);
         if (count > 3) {
           observer.error(new Error('Count is greater 3!'));
         }
@@ -32,7 +33,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      filter((data: number) => {
+        return data > 0;
+      }),
+      map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })
+    ).subscribe(data => {
       console.log('Subscription: ' + data);
     }, error => {
       console.log(error);
